0bd44f3d871d2facb04c33f23f9434ccaf5c5dfb
Ruairi O'Donohoe <58832267+ruairiodonohoe@users.noreply.github.com>
2025-06-23 17:23:33 +0100

    Add registration form

--- src/run.py
+++ src/run.py
@@ -1,1 +1,9 @@
-from microblog import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from microblog import app, db
+from microblog.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {"sa": sa, "so": so, "db": db, "User": User, "Post": Post}
