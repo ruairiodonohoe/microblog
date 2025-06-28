9e8e1e4638c9d227bb3636d3f82a9e0507721f19
Ruairi O'Donohoe <58832267+ruairiodonohoe@users.noreply.github.com>
2025-06-28 13:35:30 +0100

    edits

--- src/microblog/main/routes.py
+++ src/microblog/main/routes.py
@@ -163,7 +163,7 @@ def explore():
 @login_required
 def search():
     if not g.search_form.validate():
-        return redirect(url_for("main/explore"))
+        return redirect(url_for("main.explore"))
     page = request.args.get("page", 1, type=int)
     posts, total = Post.search(
         g.search_form.q.data, page, current_app.config["POSTS_PER_PAGE"]
